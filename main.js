93e8d0dd492218793f441ac79cc08824f8440eba
Valgard Trontheim <github@trontheim.de>
2024-10-27 19:42:19 +0100

    added feature image and text

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const sizeOf = require('image-size');
+const URLHelper = require('./helpers/url');
+const ContentHelper = require('./helpers/content');
+const Image = require('./helpers/image');
+
 class SimpleContactForm {
     constructor(API, name, config) {
         this.API = API;
@@ -10,10 +16,10 @@ class SimpleContactForm {
         this.API.addModifier('htmlOutput', this.generateContactForm, 1, this);
     }
 
-    modifyMenuStructure(rendererInstance, output, ...params) {
-        let menuPosition = parseInt(this.config['menu']);
+    modifyMenuStructure(rendererInstance, output) {
+        this.rendererInstance = rendererInstance;
 
-        const translations = rendererInstance.translations.user.contact ?? rendererInstance.translations.theme.contact;
+        const translations = this.rendererInstance.translations.user.contact ?? this.rendererInstance.translations.theme.contact;
 
         const menuItem = {
             "id": 1,
@@ -30,11 +36,11 @@ class SimpleContactForm {
             "linkID": "empty"
         }
 
+        let menuPosition = parseInt(this.config['menuPosition']);
+
         if(menuPosition < 0) {
             menuPosition = output[this.config['menu']].items.length + menuPosition
-        }
-
-        if(menuPosition > 0) {
+        } else if(menuPosition > 0) {
             menuPosition -= 1;
         }
 
@@ -44,39 +50,179 @@ class SimpleContactForm {
     }
 
     generateContactForm(rendererInstance, output, globalContext, context) {
+        this.rendererInstance = rendererInstance;
+
         if (globalContext.context.length !== 1 || globalContext.context[0] !== 'index') {
             return output;
         }
 
-        const translations = rendererInstance.translations.user.contact ?? rendererInstance.translations.theme.contact;
+        const translations = this.rendererInstance.translations.user.contact ?? this.rendererInstance.translations.theme.contact;
 
         // Load template
         let suffix = '.html';
         let pageSlug = translations.menu.slug;
         let inputFile = 'contact.hbs';
-        let compiledTemplate = rendererInstance.compileTemplate(inputFile);
+        let compiledTemplate = this.rendererInstance.compileTemplate(inputFile);
 
         if (globalContext.config.site.urls.cleanUrls) {
             suffix = '/index.html';
         }
 
+        const oldMenuContext = this.rendererInstance.menuContext
+        this.rendererInstance.menuContext = ['contact'];
+
         globalContext.context = ['contact'];
-        globalContext.plugins.simpleContactForm.config.recaptcha = this.config.recaptchaSiteKey && !rendererInstance.previewMode;
+        globalContext.plugins[this.name].config.recaptcha = this.config.recaptchaEnabled && this.config.recaptchaSiteKey && !this.rendererInstance.previewMode;
 
         context.title = `${globalContext.website.name} - ${translations.menu.label}`;
         context.contact = {
-            title: `${translations.menu.label}`
+            title: `${translations.menu.label}`,
+            featuredImage: false,
+            text: globalContext.plugins[this.name].config.contactContent,
         }
 
-        // let uuid = crypto.randomUUID()
-        // this.API.createFile(`[ROOT-FILES]/${uuid}.generateContactForm.output.json`, JSON.stringify(output), this);
-        // this.API.createFile(`[ROOT-FILES]/${uuid}.generateContactForm.globalContext.json`, JSON.stringify(globalContext), this);
-        // this.API.createFile(`[ROOT-FILES]/${uuid}.generateContactForm.context.json`, JSON.stringify(context), this);
+        if(globalContext.plugins[this.name].config['contactFeaturedImage'] !== undefined && globalContext.plugins[this.name].config.contactFeaturedImage !== '') {
+            let imageUrl = globalContext.plugins[this.name].config.contactFeaturedImage;
+            let imageData = {
+                id: path.parse(imageUrl).base,
+                url: imageUrl,
+                alt: `${translations.menu.label}`,
+            };
 
-        let content = rendererInstance.renderTemplate(compiledTemplate, context, globalContext, inputFile);
-        rendererInstance.templateHelper.saveOutputFile(pageSlug + suffix, content);
+            context.contact.featuredImage = this.getFeaturedImages(imageUrl, imageData);
+        }
+
+        let content = this.rendererInstance.renderTemplate(compiledTemplate, context, globalContext, inputFile);
+        this.rendererInstance.templateHelper.saveOutputFile(pageSlug + suffix, content);
+
+        this.rendererInstance.menuContext = oldMenuContext
 
         return output;
+    }
+
+    getFeaturedImages(imageFilename, featuredImageData) {
+        let contactImage = {
+            id: path.parse(imageFilename).base,
+            url: imageFilename,
+            additional_data: JSON.stringify(featuredImageData)
+        };
+
+        if (contactImage && contactImage.url) {
+            let imagePath = '';
+            let url = '';
+            let alt = '';
+            let caption = '';
+            let credits = '';
+            let imageDimensions = false;
+
+            if (contactImage.additional_data) {
+                let data = JSON.parse(contactImage.additional_data);
+
+                imagePath = URLHelper.createImageURL(this.rendererInstance.inputDir, this.name, contactImage.url, 'plugin');
+                let domain = this.rendererInstance.siteConfig.domain;
+
+                url = URLHelper.createImageURL(domain, this.name, contactImage.url, 'plugin');
+                alt = data.alt;
+                caption = data.caption;
+                credits = data.credits;
+
+                try {
+                    imageDimensions = sizeOf(imagePath);
+                } catch(e) {
+                    console.log('simple-contact-form.js: wrong image path - missing dimensions');
+                    imageDimensions = false;
+                }
+            } else {
+                return false;
+            }
+
+            let featuredImageSrcSet = false;
+            let featuredImageSizes = false;
+
+            if(!this.isGifOrSvg(url)) {
+                let useWebp = false;
+
+                if (this.rendererInstance.siteConfig?.advanced?.forceWebp) {
+                    useWebp = true;
+                }
+
+                featuredImageSrcSet = ContentHelper.getFeaturedImageSrcset(url, this.rendererInstance.themeConfig, useWebp);
+                featuredImageSizes = ContentHelper.getFeaturedImageSizes(this.rendererInstance.themeConfig);
+            } else {
+                featuredImageSrcSet = '';
+                featuredImageSizes = '';
+            }
+
+            let featuredImageData = {
+                id: contactImage.id,
+                url: url,
+                alt: alt,
+                caption: caption,
+                credits: credits,
+                height: imageDimensions.height,
+                width: imageDimensions.width,
+                srcset: featuredImageSrcSet,
+                sizes: featuredImageSizes
+            };
+
+            // Create responsive images
+            let featuredImage = new Image(this.rendererInstance, this, featuredImageData);
+            const proms = featuredImage.createResponsiveImages(imagePath);
+            if (proms) {
+                Promise.allSettled(proms)
+                    .catch(() => {
+                    });
+            }
+
+            // Create alternative names for dimensions
+            let dimensions = false;
+
+            if (
+                this.rendererInstance.themeConfig.files &&
+                this.rendererInstance.themeConfig.files.responsiveImages
+            ) {
+                if (
+                    this.rendererInstance.themeConfig.files.responsiveImages.featuredImages &&
+                    this.rendererInstance.themeConfig.files.responsiveImages.featuredImages.dimensions
+                ) {
+                    dimensions = this.rendererInstance.themeConfig.files.responsiveImages.featuredImages.dimensions;
+                } else if (
+                    this.rendererInstance.themeConfig.files.responsiveImages.contentImages &&
+                    this.rendererInstance.themeConfig.files.responsiveImages.contentImages.dimensions
+                ) {
+                    dimensions = this.rendererInstance.themeConfig.files.responsiveImages.featuredImages.dimensions;
+                }
+
+                if (dimensions) {
+                    let dimensionNames = Object.keys(dimensions);
+
+                    for (let dimensionName of dimensionNames) {
+                        let base = path.parse(url).base;
+                        let filename = path.parse(url).name;
+                        let extension = path.parse(url).ext;
+                        let newFilename = filename + '-' + dimensionName + extension;
+                        let capitalizedDimensionName = dimensionName.charAt(0).toUpperCase() + dimensionName.slice(1);
+
+                        if(!this.isGifOrSvg(url)) {
+                            featuredImageData['url' + capitalizedDimensionName] = url.replace(base, newFilename);
+                        } else {
+                            featuredImageData['url' + capitalizedDimensionName] = url;
+                        }
+                    }
+                }
+            }
+
+            return featuredImageData;
+        }
+
+        return false;
+    }
+
+    /**
+     * Detects if image is a GIF or SVG
+     */
+    isGifOrSvg(url) {
+        return url.slice(-4) === '.gif' || url.slice(-4) === '.svg';
     }
 }
 
